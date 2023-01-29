75491542c7b117fb69dc500c5b61d4f2b974d245
Usually-zz <yujulee@cs.toronto.edu>
2023-01-29 16:45:57 -0500

    empty

--- damm/old/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name = 'DAMM', 
-	version = '0.1', 
-	description = 'Doublet Aware Mixture Model',
-	author = 'Jett Lee',
-	packages = ['damm'],
-	zip_safe = False)

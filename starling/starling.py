941f78d850c20861a5cf9ca5cd720abab8948eb7
cklamann <12862284+cklamann@users.noreply.github.com>
2024-05-30 11:44:40 -0400

    move empty init_cell_size_centroids to uns

--- starling/starling.py
+++ starling/starling.py
@@ -160,8 +160,8 @@ class ST(pl.LightningModule):
             self.adata.uns["init_cell_size_variances"] = np.array(init_sv)
         else:
             # init_cell_size_centroids = None; init_cell_size_variances = None
-            self.adata.varm["init_cell_size_centroids"] = None
-            self.adata.varm["init_cell_size_variances"] = None
+            self.adata.uns["init_cell_size_centroids"] = None
+            self.adata.uns["init_cell_size_variances"] = None
             self.train_df = utility.ConcatDataset([self.X, tr_fy, tr_fl])
 
         # model_params = utility.model_paramters(self.init_e, self.init_v, self.init_s, self.init_sv)
@@ -397,9 +397,9 @@ class ST(pl.LightningModule):
         # else:
         c = self.model_params["log_mu"].detach().exp().cpu().numpy()
         # v = self.model_params['log_sigma'].cpu().detach().exp().cpu().numpy()
-        self.adata.varm[
-            "st_exp_centroids"
-        ] = c.T  # pd.DataFrame(c, columns=self.adata.var_names)
+        self.adata.varm["st_exp_centroids"] = (
+            c.T
+        )  # pd.DataFrame(c, columns=self.adata.var_names)
 
         if self.model_cell_size:
             self.adata.uns["st_cell_size_centroids"] = (
--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -125,3 +125,25 @@ def test_can_run_pg(tmpdir):
     )
 
     assert prom_mat.shape[0] == 13685
+
+def test_can_run_pg_without_cell_size(tmpdir):
+    """Temporary sanity check"""
+    seed_everything(10, workers=True)
+    adata = utility.init_clustering(
+        "PG",
+        ad.read_h5ad(join(dirname(__file__), "fixtures", "sample_input.h5ad")),
+        k=10,
+    )
+    st = starling.ST(adata, model_cell_size=False)
+    cb_early_stopping = EarlyStopping(monitor="train_loss", mode="min", verbose=False)
+
+    ## train ST
+    st.train_and_fit(
+        max_epochs=2,
+        callbacks=[cb_early_stopping],
+        default_root_dir=tmpdir,
+    )
+
+    result = st.result()
+
+    assert True
